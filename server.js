8d5e44dfe4620323774335ecebf8366094a018bc
Scott Maclure <scott@maclure.com.au>
2016-04-13 16:56:48 +1000

    Tweaking comments and code while I read them.

--- server.js
+++ server.js
@@ -5,11 +5,12 @@
 var express = require('express'),
 	serveStatic = require('serve-static'),
 	errorHandler = require('errorhandler'),
-	path = require('path');
+	path = require('path'),
+	indexRoute = require('./routes/index');
 
 var app = express();
 
-// For openshift.
+// Default port is 7001, else use the "PORT" environment variable.
 app.set('port', process.env.PORT || 7001);
 
 // Static file server.
@@ -18,13 +19,12 @@ app.use(serveStatic(path.join(__dirname, 'public')));
 // Required to support JSON-encoded bodies
 //app.use(express.json());
 
-// TODO Check for process.env.NODE_ENV === 'development'
+// Typically, you'd check for process.env.NODE_ENV === 'development'
+// But currently I statically generate the html and upload to github pages,
+// so I don't care.
 app.use(errorHandler());
 
-// Load routes, passing app
-var indexRoute = require('./routes/index');
-
-// Bind routes
+// Bind routes.
 app.route('/').get(indexRoute.index);
 
 // @see http://stackoverflow.com/questions/17696801/express-js-app-listen-vs-server-listen
