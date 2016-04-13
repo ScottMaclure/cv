231ab80a1de2357fc689b305da8ead04c1443bd6
Scott Maclure <scott@maclure.com.au>
2016-04-13 17:03:03 +1000

    Minor formatting.

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@
  * Default routes.
  */
 
-var fs = require('fs');
-var React = require('react');
-var ReactDOMServer = require('react-dom/server');
-var FragmentHelper = require('../libs/FragmentHelper');
-var jsonResumeComponent = React.createFactory(require('../components/JsonResume'));
+var fs = require('fs'),
+	React = require('react'),
+	ReactDOMServer = require('react-dom/server'),
+	FragmentHelper = require('../libs/FragmentHelper'),
+	jsonResumeComponent = React.createFactory(require('../components/JsonResume'));
 
 exports.index = function (req, res) {
 
-	// TODO Dynamic, cached.
+	// TODO Since we use nodemon, do we need this to be loaded every request?
 	var jsonResume = JSON.parse(fs.readFileSync(__dirname + '/../data/scott-maclure.json', 'utf-8'));
 	var pkg = require('../package.json');
 
