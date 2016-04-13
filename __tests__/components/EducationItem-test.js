bf1ba5f61c3fa0aa2650a432939a1ebbf3573a38
Scott Maclure <scott@maclure.com.au>
2016-04-13 18:07:58 +1000

    Fixed first out-dated test code.

--- __tests__/components/EducationItem-test.js
+++ __tests__/components/EducationItem-test.js
@@ -1,9 +1,9 @@
 jest.dontMock('../../components/EducationItem.js');
 
-var React = require('react/addons');
-var TestUtils = React.addons.TestUtils;
+var React = require('react');
+var TestUtils = require('react-addons-test-utils');
 
-var educationItemComponent = require('../../components/EducationItem');
+var educationItemComponent = React.createFactory(require('../../components/EducationItem'));
 
 describe('EducationItem component test suite', function () {
 
