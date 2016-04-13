879449e02470dd4b892d85dafce0d206831a3b97
Scott Maclure <scott@maclure.com.au>
2016-04-13 21:25:41 +1000

    Update component to pass tests.

--- __tests__/components/Footer-test.js
+++ __tests__/components/Footer-test.js
@@ -1,6 +1,7 @@
 jest.dontMock('../../components/Footer.js');
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
 
 var footerComponent = React.createFactory(require('../../components/Footer.js'));
@@ -30,21 +31,21 @@ describe('Footer component test suite', function () {
 
 		it('renders author name', function () {
 			var domCom = TestUtils.findRenderedDOMComponentWithClass(component, 'footer__author');
-			expect(domCom.getDOMNode().textContent).toEqual('TestAuthor');
+			expect(ReactDOM.findDOMNode(domCom).textContent).toEqual('TestAuthor');
 		});
 
 		it('renders email', function () {
 			var domCom = TestUtils.findRenderedDOMComponentWithClass(component, 'footer__author');
-			expect(domCom.getDOMNode().href).toEqual('mailto:test@email.com');
+			expect(ReactDOM.findDOMNode(domCom).href).toEqual('mailto:test@email.com');
 		});
 
 		it('renders github link', function () {
-			var domCom = TestUtils.findRenderedDOMComponentWithClass(component, 'footer_url');
-			expect(domCom.getDOMNode().href).toEqual('https://github.com/ScottMaclure/scott-cv/');
+			var domCom = TestUtils.findRenderedDOMComponentWithClass(component, 'footer__url');
+			expect(ReactDOM.findDOMNode(domCom).href).toEqual('https://github.com/ScottMaclure/scott-cv/');
 		});
 
 		it('renders the CV version', function () {
-			var domCom = TestUtils.scryRenderedDOMComponentsWithClass(component, 'footer_version');
+			var domCom = TestUtils.scryRenderedDOMComponentsWithClass(component, 'footer__version');
 			expect(domCom.length).toBe(1);
 		});
 
