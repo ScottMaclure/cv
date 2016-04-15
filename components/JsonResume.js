1335962a8452a24d0f8cbe5dba15ba11e60c12c4
Scott Maclure <scott@maclure.com.au>
2016-04-15 13:55:46 +1000

    [#11] Update component with key props.

--- components/JsonResume.js
+++ components/JsonResume.js
@@ -20,41 +20,42 @@ module.exports = React.createClass({
 		var components = [];
 
 		components.push(
-			DOM.div({ className: 'page-header' }, [
+			DOM.div({ key: 'header', className: 'page-header' },
 				DOM.div({ className: 'row' }, [
-					DOM.div({ className: 'col-xs-12 col-sm-10' },
+					DOM.div({ key: 'headerText', className: 'col-xs-12 col-sm-10' },
 						DOM.h1(null, [
-							DOM.div(null, this.props.jsonResume.basics.name),
-							DOM.small(null, ' ' + this.props.jsonResume.basics.label)
+							DOM.div({key: 'name'}, this.props.jsonResume.basics.name),
+							DOM.small({key: 'label'}, ' ' + this.props.jsonResume.basics.label)
 						])
 					),
-					DOM.div({ className: 'col-xs-12 col-sm-2 hidden-xs text-right' },
+					DOM.div({ key: 'headerAvatar', className: 'col-xs-12 col-sm-2 hidden-xs text-right' },
 						DOM.img({ className: 'gravatar', src: this.props.jsonResume.basics.picture })
 					)
 				])
-			])
+			)
 		);
 
 		components.push(
-			plainTextComponent({ title: 'About', body: this.props.jsonResume.basics.summary })
+			plainTextComponent({ key: 'summary', title: 'About', body: this.props.jsonResume.basics.summary })
 		);
 
-		components.push(skillsComponent({ title: 'Skills', items: this.props.jsonResume.skills }));
+		components.push(skillsComponent({ key: 'skills', title: 'Skills', items: this.props.jsonResume.skills }));
 
-		components.push(workComponent({ title: 'Work', work: this.props.jsonResume.work }));
+		components.push(workComponent({ key: 'work', title: 'Work', work: this.props.jsonResume.work }));
 
 		// Education
 		// TODO Create an Education component and move this in there. Or be generic?
 		components.push(
-			DOM.div({ className: 'educationContainer' }, [
-				DOM.div({ className: 'row' },
+			DOM.div({ key: 'education', className: 'educationContainer' }, [
+				DOM.div({ key: 'educationTitle', className: 'row' },
 					DOM.div({ className: 'col-xs-12' },
 						DOM.h2(null, 'Education')
 					)
 				),
-				DOM.div({ className: 'row' },
+				DOM.div({ key: 'educationBody', className: 'row' },
 					DOM.div({ className: 'col-xs-12' },
-						this.props.jsonResume.education.map(function (education) {
+						this.props.jsonResume.education.map(function (education, index) {
+							education.key = 'education' + index;
 							return educationItemComponent(education);
 						})
 					)
@@ -65,6 +66,7 @@ module.exports = React.createClass({
 		// Optional, human languages component.
 		if (this.props.jsonResume.languages && this.props.jsonResume.languages.length > 0) {
 			components.push(languagesComponent({
+				key: 'languages',
 				languages: this.props.jsonResume.languages
 			}));
 		}
@@ -72,6 +74,7 @@ module.exports = React.createClass({
 		// Footer
 		components.push(
 			footerComponent({
+				key: 'footer',
 				version: this.props.pkg.version,
 				author: this.props.jsonResume.basics.name,
 				email: this.props.jsonResume.basics.email,
